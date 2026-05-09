c5ded4853a7e5bed7949665dcdce4ed36d441613
kouki08 <kouki.e.0808@gmail.com>
2026-05-09 20:17:06 +0900

    blog追加

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// ページトップ・お問い合わせボタン（FVを超えたら表示）
+const $pagetop = $('#js-pagetop');
+const $contactBtn = $('.contact-btn');
+const fvHeight = $('.fv').outerHeight();
+
+$(window).on('scroll', function() {
+    if ($(this).scrollTop() > fvHeight) {
+        $pagetop.addClass('is-show');
+        $contactBtn.addClass('is-show');
+    } else {
+        $pagetop.removeClass('is-show');
+        $contactBtn.removeClass('is-show');
+    }
+});
+
+$pagetop.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 500);
+});
+
 $('.qa-title').on('click', function(){
     $(this).toggleClass('active');
     var $next = $(this).next('.qa-text');
